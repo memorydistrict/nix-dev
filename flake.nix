b8bbc0c2b1d63c3f5f8f3fb64d694e1104875b4d
memorydistrict <80621403+memorydistrict@users.noreply.github.com>
2025-07-22 21:40:29 +1000

    created nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+	description = "Development environments";
+
+	inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+	outputs = { nixpkgs, ... }: let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in {
+		devShells.${system}.nodejs = pkgs.mkShell {
+			packages = [
+				pkgs.nodejs_latest
+				pkgs.fish
+			];
+			shellHook = ''
+				exec fish
+			'';
+		};
+	};
+}
+
+# --------------------------- #
+# Vim modline (do not remove)
+# vim: ts=4 sts=4 sw=4
